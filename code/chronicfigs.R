5760df583a6d629c719d6f41f355b78569b1ad0e
skissler <skissler@hsph.harvard.edu>
2022-09-05 18:04:01 -0400

    update

--- code/chronicfigs.R
+++ code/chronicfigs.R
@@ -14,7 +14,14 @@ memb_df_chronic <- memb_df[ENROLID %in% chronic_ids]
 rx_df_chronic <- rx_df[ENROLID %in% chronic_ids]
 visit_df_chronic <- visit_df[ENROLID %in% chronic_ids]
 
-# Count cumulative prescriptions across all conditions: 
+memb_df_nonchronic <- memb_df[!(ENROLID %in% chronic_ids)]
+rx_df_nonchronic <- rx_df[!(ENROLID %in% chronic_ids)]
+visit_df_nonchronic <- visit_df[!(ENROLID %in% chronic_ids)]
+
+# Count cumulative prescriptions for people with chronic conditions: 
+rx_df_chronic <- visit_df_chronic[,.(ASSOC_VISIT_ID=ID,COND)][
+	rx_df_chronic, on=.(ASSOC_VISIT_ID)]
+
 cumrx_df_chronic <- setDT(data.frame())
 for(t in seq(from=0, to=1825, by=5)){
 	cumrx_df_chronic <- rbind(cumrx_df_chronic,
@@ -22,33 +29,19 @@ for(t in seq(from=0, to=1825, by=5)){
 }
 cumrx_df_chronic <- cumrx_df_chronic[,NMEMB:=nrow(memb_df_chronic)]
 
-# Count cumulative prescriptions for respiratory conditions: 
-rx_df_chronic_resp <- visit_df_chronic[,.(ASSOC_VISIT_ID=ID,COND)][
-	rx_df_chronic, on=.(ASSOC_VISIT_ID)][
-	COND %in% c("Sinusitis","Strep pharyngitis","Pneumonia","Influenza","Tonsillitis","Bronchitis (acute)","URI (other)","Otitis media")]
+# Count cumulative prescriptions for people with nonchronic conditions: 
+rx_df_nonchronic <- visit_df_nonchronic[,.(ASSOC_VISIT_ID=ID,COND)][
+	rx_df_nonchronic, on=.(ASSOC_VISIT_ID)]
 
-cumrx_df_chronic_resp <- setDT(data.frame())
+cumrx_df_nonchronic <- setDT(data.frame())
 for(t in seq(from=0, to=1825, by=5)){
-	cumrx_df_chronic_resp <- rbind(cumrx_df_chronic_resp,
-		rx_df_chronic_resp[AGE_DAYS<=t,.(NOBS=.N, NRX=.N/nrow(memb_df_chronic),AGE_DAYS_ROUNDED=t)])
+	cumrx_df_nonchronic <- rbind(cumrx_df_nonchronic,
+		rx_df_nonchronic[AGE_DAYS<=t,.(NOBS=.N, NRX=.N/nrow(memb_df_nonchronic),AGE_DAYS_ROUNDED=t)])
 }
-cumrx_df_chronic_resp <- cumrx_df_chronic_resp[,NMEMB:=nrow(memb_df_chronic)]
-
-# Count cumulative prescriptions for non-respiratory conditions: 
-rx_df_chronic_nonresp <- visit_df_chronic[,.(ASSOC_VISIT_ID=ID,COND)][
-	rx_df_chronic, on=.(ASSOC_VISIT_ID)][
-	!(COND %in% c("Sinusitis","Strep pharyngitis","Pneumonia","Influenza","Tonsillitis","Bronchitis (acute)","URI (other)","Otitis media"))]
-
-cumrx_df_chronic_nonresp <- setDT(data.frame())
-for(t in seq(from=0, to=1825, by=5)){
-	cumrx_df_chronic_nonresp <- rbind(cumrx_df_chronic_nonresp,
-		rx_df_chronic_nonresp[AGE_DAYS<=t,.(NOBS=.N, NRX=.N/nrow(memb_df_chronic),AGE_DAYS_ROUNDED=t)])
-}
-cumrx_df_chronic_nonresp <- cumrx_df_chronic_nonresp[,NMEMB:=nrow(memb_df_chronic)]
+cumrx_df_nonchronic <- cumrx_df_nonchronic[,NMEMB:=nrow(memb_df_nonchronic)]
 
 
-# Gather the data into a useful format and append confidence intervals: 
-fulldat_cumrx_chronic <- cumrx_df_chronic %>% 
+part1_cumrx_chronic <- cumrx_df_chronic %>% 
 	mutate(rawmean=NOBS/NMEMB) %>% 
 	mutate(rawlwr=qgamma(alphasig/2,NOBS,1)/NMEMB) %>%
 	mutate(rawupr=qgamma(1-alphasig/2,NOBS+1,1)/NMEMB) %>%
@@ -57,16 +50,7 @@ fulldat_cumrx_chronic <- cumrx_df_chronic %>%
 	mutate(lwr=NRX-proplwr*NRX) %>% 
 	mutate(upr=NRX+propupr*NRX) 
 
-respdat_cumrx_chronic <- cumrx_df_chronic_resp %>% 
-	mutate(rawmean=NOBS/NMEMB) %>% 
-	mutate(rawlwr=qgamma(alphasig/2,NOBS,1)/NMEMB) %>%
-	mutate(rawupr=qgamma(1-alphasig/2,NOBS+1,1)/NMEMB) %>%
-	mutate(proplwr=(rawmean-rawlwr)/rawmean) %>% 
-	mutate(propupr=(rawupr-rawmean)/rawmean) %>% 
-	mutate(lwr=NRX-proplwr*NRX) %>% 
-	mutate(upr=NRX+propupr*NRX) 
-
-nonrespdat_cumrx_chronic <- cumrx_df_chronic_nonresp %>% 
+part2_cumrx_chronic <- cumrx_df_nonchronic %>% 
 	mutate(rawmean=NOBS/NMEMB) %>% 
 	mutate(rawlwr=qgamma(alphasig/2,NOBS,1)/NMEMB) %>%
 	mutate(rawupr=qgamma(1-alphasig/2,NOBS+1,1)/NMEMB) %>%
@@ -76,22 +60,21 @@ nonrespdat_cumrx_chronic <- cumrx_df_chronic_nonresp %>%
 	mutate(upr=NRX+propupr*NRX) 
 
 combdat_cumrx_chronic <- bind_rows(
-	mutate(fulldat_cumrx_chronic,Indication="All conditions"),
-	mutate(respdat_cumrx_chronic,Indication="Respiratory conditions"),
-	mutate(nonrespdat_cumrx_chronic,Indication="Non-respiratory conditions")
+	mutate(part1_cumrx_chronic,Comorbidities="Yes"),
+	mutate(part2_cumrx_nonchronic,Comorbidities="No")
 	)
 
 # Plot cumulative prescriptions overall and by respiratory/non-respiratory conditions: 
-fig_cumrx_respnonresp_chronic <-
-	ggplot(data=combdat_cumrx_chronic, aes(x=AGE_DAYS_ROUNDED, y=NRX, col=Indication, fill=Indication, linetype=Indication)) + 
+fig_cumrx_chronic <-
+	ggplot(data=combdat_cumrx_chronic, aes(x=AGE_DAYS_ROUNDED, y=NRX, col=Comorbidities, fill=Comorbidities, linetype=Comorbidities)) + 
 		geom_ribbon(aes(ymin=lwr,ymax=upr),alpha=0.4) + 
 		geom_line() + 
 		scale_x_continuous(breaks=seq(from=0, to=1825, by=365)) + 
 		theme_classic() + 
 		labs(tag="A)", x="Days from birth", y=paste0("Cumulative prescriptions")) + 
-		scale_color_manual(values=c("All conditions"="Gray","Respiratory conditions"="Blue","Non-respiratory conditions"="Blue")) + 
-		scale_fill_manual(values=c("All conditions"="Gray","Respiratory conditions"="Blue","Non-respiratory conditions"="Blue")) +
-		scale_linetype_manual(values=c("All conditions"="solid","Respiratory conditions"="solid","Non-respiratory conditions"="dashed")) +
+		scale_color_manual(values=c("Yes"="Red","No"="Black")) + 
+		scale_fill_manual(values=c("Yes"="Red","No"="Black")) +
+		scale_linetype_manual(values=c("Yes"="solid","No"="solid")) +
 		theme(text=element_text(size=10))
 
 # ggsave(fig_cumrx_respnonresp_chronic, file="figures/cumrx.pdf", width=figwidth, height=figwidth, dpi=figres)
@@ -100,14 +83,14 @@ fig_cumrx_respnonresp_chronic <-
 # ggsave(fig_cumrx_respnonresp_chronic + theme(legend.position='none'), file="figures/cumrx_nokey.pdf", width=figwidth, height=figwidth, dpi=figres)
 # ggsave(fig_cumrx_respnonresp_chronic + theme(legend.position='none'), file="figures/cumrx_nokey.png", width=figwidth, height=figwidth, dpi=figres)
 
-fig_cumrx_comparison_chronic <-
-	ggplot(data=bind_rows(mutate(fulldat_cumrx,CLASS="NONCHRONIC"), mutate(fulldat_cumrx_chronic,class="CHRONIC")), aes(x=AGE_DAYS_ROUNDED, y=NRX, col=CLASS, fill=CLASS, linetype=CLASS)) + 
-		geom_ribbon(aes(ymin=lwr,ymax=upr),alpha=0.4) + 
-		geom_line() + 
-		scale_x_continuous(breaks=seq(from=0, to=1825, by=365)) + 
-		theme_classic() + 
-		labs(tag="A)", x="Days from birth", y=paste0("Cumulative prescriptions")) + 
-		scale_color_manual(values=c("NONCHRONIC"="Gray","CHRONIC"="Red")) + 
-		scale_fill_manual(values=c("NONCHRONIC"="Gray","CHRONIC"="Red")) +
-		scale_linetype_manual(values=c("NONCHRONIC"="solid","CHRONIC"="solid")) +
-		theme(text=element_text(size=10))
+# fig_cumrx_comparison_chronic <-
+# 	ggplot(data=bind_rows(mutate(fulldat_cumrx,Comorbidities="No"), mutate(fulldat_cumrx_chronic,Comorbidities="Yes")), aes(x=AGE_DAYS_ROUNDED, y=NRX, col=Comorbidities, fill=Comorbidities, linetype=Comorbidities)) + 
+# 		geom_ribbon(aes(ymin=lwr,ymax=upr),alpha=0.4) + 
+# 		geom_line() + 
+# 		scale_x_continuous(breaks=seq(from=0, to=1825, by=365)) + 
+# 		theme_classic() + 
+# 		labs(tag="A)", x="Days from birth", y=paste0("Cumulative prescriptions")) + 
+# 		scale_color_manual(values=c("Yes"="Red","No"="Black")) + 
+# 		scale_fill_manual(values=c("Yes"="Red","No"="Black")) +
+# 		scale_linetype_manual(values=c("Yes"="Red","No"="Black")) +
+# 		theme(text=element_text(size=10))
--- code/temp2.R
+++ code/temp2.R
@@ -1,46 +1,46 @@
 
 # Get the age of first prescription for any condition: 
-firstrx_df_superuser <- rx_df_superuser[
+firstrx_df_chronic <- rx_df_chronic[
 	,.(WEIGHT_INDIV_NOYEAR=first(WEIGHT_INDIV_NOYEAR), AGE_DAYS=min(AGE_DAYS)), by=.(ENROLID)]
 
-cumfirstrx_df_superuser <- setDT(data.frame())
+cumfirstrx_df_chronic <- setDT(data.frame())
 for(t in seq(from=0, to=1825, by=5)){
-	cumfirstrx_df_superuser <- rbind(cumfirstrx_df_superuser,
-		firstrx_df_superuser[AGE_DAYS<=t,.(NOBS=.N,NRX=.N/nrow(memb_df_superuser),AGE_DAYS_ROUNDED=t)])
+	cumfirstrx_df_chronic <- rbind(cumfirstrx_df_chronic,
+		firstrx_df_chronic[AGE_DAYS<=t,.(NOBS=.N,NRX=.N/nrow(memb_df_chronic),AGE_DAYS_ROUNDED=t)])
 }
-cumfirstrx_df_superuser <- cumfirstrx_df_superuser[,NMEMB:=nrow(memb_df_superuser)]
+cumfirstrx_df_chronic <- cumfirstrx_df_chronic[,NMEMB:=nrow(memb_df_chronic)]
 
 
 # Get the age of first prescription for a respiratory condition: 
-firstrx_df_superuser_resp <- visit_df_superuser[,.(ASSOC_VISIT_ID=ID,COND)][
-	rx_df_superuser, on=.(ASSOC_VISIT_ID)][
+firstrx_df_chronic_resp <- visit_df_chronic[,.(ASSOC_VISIT_ID=ID,COND)][
+	rx_df_chronic, on=.(ASSOC_VISIT_ID)][
 	COND %in% c("Sinusitis","Strep pharyngitis","Pneumonia","Influenza","Tonsillitis","Bronchitis (acute)","URI (other)","Otitis media")][
 	,.(WEIGHT_INDIV_NOYEAR=first(WEIGHT_INDIV_NOYEAR), AGE_DAYS=min(AGE_DAYS)), by=.(ENROLID)]
 
-cumfirstrx_df_superuser_resp <- setDT(data.frame())
+cumfirstrx_df_chronic_resp <- setDT(data.frame())
 for(t in seq(from=0, to=1825, by=5)){
-	cumfirstrx_df_superuser_resp <- rbind(cumfirstrx_df_superuser_resp,
-		firstrx_df_superuser_resp[AGE_DAYS<=t,.(NOBS=.N,NRX=.N/nrow(memb_df_superuser),AGE_DAYS_ROUNDED=t)])
+	cumfirstrx_df_chronic_resp <- rbind(cumfirstrx_df_chronic_resp,
+		firstrx_df_chronic_resp[AGE_DAYS<=t,.(NOBS=.N,NRX=.N/nrow(memb_df_chronic),AGE_DAYS_ROUNDED=t)])
 }
-cumfirstrx_df_superuser_resp <- cumfirstrx_df_superuser_resp[,NMEMB:=nrow(memb_df_superuser)]
+cumfirstrx_df_chronic_resp <- cumfirstrx_df_chronic_resp[,NMEMB:=nrow(memb_df_chronic)]
 
 
 # Get the age of first prescription for a non-respiratory condition: 
-firstrx_df_superuser_nonresp <- visit_df_superuser[,.(ASSOC_VISIT_ID=ID,COND)][
-	rx_df_superuser, on=.(ASSOC_VISIT_ID)][
+firstrx_df_chronic_nonresp <- visit_df_chronic[,.(ASSOC_VISIT_ID=ID,COND)][
+	rx_df_chronic, on=.(ASSOC_VISIT_ID)][
 	!(COND %in% c("Sinusitis","Strep pharyngitis","Pneumonia","Influenza","Tonsillitis","Bronchitis (acute)","URI (other)","Otitis media"))][
 	,.(WEIGHT_INDIV_NOYEAR=first(WEIGHT_INDIV_NOYEAR), AGE_DAYS=min(AGE_DAYS)), by=.(ENROLID)]
 
-cumfirstrx_df_superuser_nonresp <- setDT(data.frame())
+cumfirstrx_df_chronic_nonresp <- setDT(data.frame())
 for(t in seq(from=0, to=1825, by=5)){
-	cumfirstrx_df_superuser_nonresp <- rbind(cumfirstrx_df_superuser_nonresp,
-		firstrx_df_superuser_nonresp[AGE_DAYS<=t,.(NOBS=.N,NRX=.N/nrow(memb_df_superuser),AGE_DAYS_ROUNDED=t)])
+	cumfirstrx_df_chronic_nonresp <- rbind(cumfirstrx_df_chronic_nonresp,
+		firstrx_df_chronic_nonresp[AGE_DAYS<=t,.(NOBS=.N,NRX=.N/nrow(memb_df_chronic),AGE_DAYS_ROUNDED=t)])
 }
-cumfirstrx_df_superuser_nonresp <- cumfirstrx_df_superuser_nonresp[,NMEMB:=nrow(memb_df_superuser)]
+cumfirstrx_df_chronic_nonresp <- cumfirstrx_df_chronic_nonresp[,NMEMB:=nrow(memb_df_chronic)]
 
 
 # Gather the data into a useful format and append confidence intervals: 
-fulldat_firstcumrx <- cumfirstrx_df_superuser %>% 
+fulldat_firstcumrx_chronic <- cumfirstrx_df_chronic %>% 
 	mutate(rawmean=NOBS/NMEMB) %>% 
 	mutate(rawlwr=qgamma(alphasig/2,NOBS,1)/NMEMB) %>%
 	mutate(rawupr=qgamma(1-alphasig/2,NOBS+1,1)/NMEMB) %>%
@@ -49,7 +49,7 @@ fulldat_firstcumrx <- cumfirstrx_df_superuser %>%
 	mutate(lwr=NRX-proplwr*NRX) %>% 
 	mutate(upr=NRX+propupr*NRX) 
 
-respdat_firstcumrx <- cumfirstrx_df_superuser_resp %>% 
+respdat_firstcumrx_chronic <- cumfirstrx_df_chronic_resp %>% 
 	mutate(rawmean=NOBS/NMEMB) %>% 
 	mutate(rawlwr=qgamma(alphasig/2,NOBS,1)/NMEMB) %>%
 	mutate(rawupr=qgamma(1-alphasig/2,NOBS+1,1)/NMEMB) %>%
@@ -58,7 +58,7 @@ respdat_firstcumrx <- cumfirstrx_df_superuser_resp %>%
 	mutate(lwr=NRX-proplwr*NRX) %>% 
 	mutate(upr=NRX+propupr*NRX) 
 
-nonrespdat_firstcumrx <- cumfirstrx_df_superuser_nonresp %>% 
+nonrespdat_firstcumrx_chronic <- cumfirstrx_df_chronic_nonresp %>% 
 	mutate(rawmean=NOBS/NMEMB) %>% 
 	mutate(rawlwr=qgamma(alphasig/2,NOBS,1)/NMEMB) %>%
 	mutate(rawupr=qgamma(1-alphasig/2,NOBS+1,1)/NMEMB) %>%
@@ -67,15 +67,15 @@ nonrespdat_firstcumrx <- cumfirstrx_df_superuser_nonresp %>%
 	mutate(lwr=NRX-proplwr*NRX) %>% 
 	mutate(upr=NRX+propupr*NRX) 
 
-combdat_firstcumrx <- bind_rows(
-	mutate(fulldat_firstcumrx,Indication="All conditions"),
-	mutate(respdat_firstcumrx,Indication="Respiratory conditions"),
-	mutate(nonrespdat_firstcumrx,Indication="Non-respiratory conditions")
+combdat_firstcumrx_chronic <- bind_rows(
+	mutate(fulldat_firstcumrx_chronic,Indication="All conditions"),
+	mutate(respdat_firstcumrx_chronic,Indication="Respiratory conditions"),
+	mutate(nonrespdat_firstcumrx_chronic,Indication="Non-respiratory conditions")
 	) 
 
 # Plot age at first prescriptions overall and by respiratory/non-respiratory conditions: 
 fig_cumfirstrx_respnonresp <-
-	ggplot(data=combdat_firstcumrx, aes(x=AGE_DAYS_ROUNDED, y=NRX, col=Indication, fill=Indication, linetype=Indication)) + 
+	ggplot(data=combdat_firstcumrx_chronic, aes(x=AGE_DAYS_ROUNDED, y=NRX, col=Indication, fill=Indication, linetype=Indication)) + 
 		geom_ribbon(aes(ymin=lwr,ymax=upr),alpha=0.4) + 
 		geom_line() + 
 		scale_x_continuous(breaks=seq(from=0, to=1825, by=365)) + 
@@ -87,13 +87,21 @@ fig_cumfirstrx_respnonresp <-
 		scale_linetype_manual(values=c("All conditions"="solid","Respiratory conditions"="solid","Non-respiratory conditions"="dashed")) +
 		theme(text=element_text(size=10))
 
-ggsave(fig_cumfirstrx_respnonresp, file="figures/firstrx.pdf", width=figwidth, height=figwidth, dpi=figres)
-ggsave(fig_cumfirstrx_respnonresp, file="figures/firstrx.png", width=figwidth, height=figwidth, dpi=figres)
+# ggsave(fig_cumfirstrx_respnonresp, file="figures/firstrx.pdf", width=figwidth, height=figwidth, dpi=figres)
+# ggsave(fig_cumfirstrx_respnonresp, file="figures/firstrx.png", width=figwidth, height=figwidth, dpi=figres)
 
-ggsave(fig_cumfirstrx_respnonresp+theme(legend.position="none"), file="figures/firstrx_nokey.pdf", width=figwidth, height=figwidth, dpi=figres)
-ggsave(fig_cumfirstrx_respnonresp+theme(legend.position="none"), file="figures/firstrx_nokey.png", width=figwidth, height=figwidth, dpi=figres)
+# ggsave(fig_cumfirstrx_respnonresp+theme(legend.position="none"), file="figures/firstrx_nokey.pdf", width=figwidth, height=figwidth, dpi=figres)
+# ggsave(fig_cumfirstrx_respnonresp+theme(legend.position="none"), file="figures/firstrx_nokey.png", width=figwidth, height=figwidth, dpi=figres)
 
-# Extract proportion of children who have received a prescription by age 5: 
-combdat_firstcumrx %>% 	
-	filter(AGE_DAYS_ROUNDED==1825) %>% 
-	select(AGE_DAYS_ROUNDED, Indication, NRX, lwr, upr)
+fig_cumfirstrx_comparison_chronic <-
+	ggplot(data=bind_rows(mutate(fulldat_firstcumrx,Comorbidities="No"), mutate(fulldat_firstcumrx_chronic,Comorbidities="Yes")), aes(x=AGE_DAYS_ROUNDED, y=NRX, col=Indication, fill=Indication, linetype=Indication)) + 
+		geom_ribbon(aes(ymin=lwr,ymax=upr),alpha=0.4) + 
+		geom_line() + 
+		scale_x_continuous(breaks=seq(from=0, to=1825, by=365)) + 
+		scale_y_continuous(limits=c(0,1), breaks=seq(from=0, to=1, by=0.2)) + 
+		theme_classic() + 
+		labs(tag="B)",x="Days from birth", y=paste0("Proportion who have received a prescription")) + 
+		scale_color_manual(values=c("All conditions"="Gray","Respiratory conditions"="Blue","Non-respiratory conditions"="Blue")) + 
+		scale_fill_manual(values=c("All conditions"="Gray","Respiratory conditions"="Blue","Non-respiratory conditions"="Blue")) +
+		scale_linetype_manual(values=c("All conditions"="solid","Respiratory conditions"="solid","Non-respiratory conditions"="dashed")) +
+		theme(text=element_text(size=10))
