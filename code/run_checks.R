30b77ce72eba15717284b4d230d2d45b0c48a0b0
Stephen Kissler <38704110+skissler@users.noreply.github.com>
2022-09-12 12:56:17 -0400

    update

---
+++ code/run_checks.R
@@ -0,0 +1,9 @@
+# Diagnosis codes in positions 3 and 4: ----------------------------------------
+
+
+
+# Proportion of kids w/ chronic conditions in the top 20% of abx recipients: ---
+
+
+
+# Venue of antibiotic prescribing: ---------------------------------------------
