433b9546820560be5e80720389e056ea2b8ce0a1
Stephen Kissler <38704110+skissler@users.noreply.github.com>
2022-09-12 13:00:31 -0400

    update

--- code/run_checks.R
+++ code/run_checks.R
@@ -1,6 +1,7 @@
 # Diagnosis codes in positions 3 and 4: ----------------------------------------
 
-
+nrow(visit_df)
+nrow(visit_df[!is.na(DX1)])
 
 # Proportion of kids w/ chronic conditions in the top 20% of abx recipients: ---
 
